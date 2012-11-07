6575224a1327df848878184cfe79d210a34b858a
Fatih Erikli <fatiherikli@gmail.com>
2012-11-07 05:02:23 +0200

    fixed deep copy problem for table mockup

--- web/usemockups/usemockups/static/js/models/toolbox.js
+++ web/usemockups/usemockups/static/js/models/toolbox.js
@@ -9,7 +9,18 @@ usemockups.models.Tool = Backbone.Model.extend({
     get_attributes: function (mockup) {
         var attributes = {};
         _.forEach(this.get("attributes"), function (attribute) {
-            attributes[attribute.name] = (mockup || this).get(attribute.name) || attribute.default;
+            var default_value;
+
+            if (_.isArray(attribute.default)) {
+                // deep copy for multi dimensional arrays.
+                // http://stackoverflow.com/a/817050/498402
+                default_value = $.extend(true, [], attribute.default);
+            } else {
+                default_value = attribute.default;
+            }
+
+            attributes[attribute.name] = (mockup || this).get(attribute.name) || default_value;
+
         }, this);
         return attributes;
     }
