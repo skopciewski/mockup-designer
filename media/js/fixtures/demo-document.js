2036ba636f989d30e057c76d09f32d1c69064dc9
Fatih Erikli <fatiherikli@gmail.com>
2012-11-18 10:36:09 +0200

    changed demo document

--- media/js/fixtures/demo-document.js
+++ media/js/fixtures/demo-document.js
@@ -1,14 +1,10 @@
 usemockups.fixtures.demo_document = {
     title:"Demo document",
-    mockups: [
-        {"top":129, "left":165, "z_index":3, "tool":"text", "label":true, "text":"Username", "width":200, "height":30},
-        {"top":205, "left":166, "z_index":3, "tool":"text", "label":true, "text":"Password", "width":200, "height":30},
-        {"top":284, "left":165, "z_index":3, "tool":"checkbox", "checked":true, "text":"Remember me"},
-        {"top":328, "left":165, "z_index":3, "tool":"button", "text":"Login", "width":80},
-        {"top":376, "left":166, "z_index":3, "tool":"label", "text":"I forgot my password", "size":"16", "color":"blue"},
-        {"top":97, "left":140, "z_index":2, "tool":"rectangle", "width":257, "height":319, "color":"#dedede", "border":true},
-        {"top":76, "left":161, "z_index":4, "tool":"rectangle", "width":134, "height":30, "color":"#dedede", "border":true},
-        {"top":82, "left":174, "z_index":5, "tool":"label", "text":"Login form", "size":"17", "color":"black"},
-        {"top":95, "left":446, "z_index":0, "tool":"paragraph", "text":"This is just a demo document.", "width":300, "background":"#dedede"}
+    mockups:[
+        {"top":132, "left":165, "z_index":3, "tool":"text", "label":true, "text":"Username", "width":200, "height":30},
+        {"top":205, "left":167, "z_index":3, "tool":"text", "label":true, "text":"Password", "width":200, "height":30},
+        {"top":283, "left":168, "z_index":3, "tool":"checkbox", "checked":true, "text":"Remember me"},
+        {"top":324, "left":167, "z_index":3, "tool":"button", "text":"Login", "width":80},
+        {"top":372, "left":169, "z_index":3, "tool":"label", "text":"I forgot my password", "size":"16", "color":"blue"}
     ]
 }
